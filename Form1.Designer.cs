a1abd4dd79f19ec199b7179c7a1101eba74e8fc8
Lectorem794643 <79637710149@mail.ru>
2023-02-13 20:57:57 +0300

    Прописал в ручную свою первую табличку, запретил пользователю ее редактировать, добавил иконку

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NewTestWindowsForms
 {
-    partial class Form1
+    partial class TestNewTable
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,20 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TestNewTable));
+            this.newTable = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.newTable)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // newTable
+            // 
+            this.newTable.AllowUserToAddRows = false;
+            this.newTable.AllowUserToDeleteRows = false;
+            this.newTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.newTable.Location = new System.Drawing.Point(12, 12);
+            this.newTable.Name = "newTable";
+            this.newTable.ReadOnly = true;
+            this.newTable.Size = new System.Drawing.Size(776, 426);
+            this.newTable.TabIndex = 0;
+            // 
+            // TestNewTable
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.newTable);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TestNewTable";
+            this.Text = "TestNewTable";
+            ((System.ComponentModel.ISupportInitialize)(this.newTable)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView newTable;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,75 @@ using System.Windows.Forms;
 
 namespace NewTestWindowsForms
 {
-    public partial class Form1 : Form
+    public partial class TestNewTable : Form
     {
-        public Form1()
+        public TestNewTable()
         {
             InitializeComponent();
+            startNewTable();
+        }
+
+        /* Отправил на Git 13.02.2023 */
+
+        public void startNewTable()
+        {
+            /* Сначала задаю струкутру своей таблицы и прописываю имя и название ячеек */
+
+            // создаем колонку 1 - Фамилия
+            DataGridViewTextBoxColumn coloum_0 = new DataGridViewTextBoxColumn();
+            coloum_0.Name = "Name1";
+            coloum_0.HeaderText = "Фамилия";
+
+            // создаем колонку 2 - Имя
+            DataGridViewTextBoxColumn coloum_1 = new DataGridViewTextBoxColumn();
+            coloum_1.Name = "Name2";
+            coloum_1.HeaderText = "Имя";
+
+            // создаем колонку 3 - Отчетсво
+            DataGridViewTextBoxColumn coloum_2 = new DataGridViewTextBoxColumn();
+            coloum_2.Name = "Name3";
+            coloum_2.HeaderText = "Отчество";
+             
+            // создаем колонку 4 - Личный код
+            DataGridViewTextBoxColumn coloum_3 = new DataGridViewTextBoxColumn();
+            coloum_3.Name = "ServiceInformation";
+            coloum_3.HeaderText = "Личный код";
+
+            // Заносим созданные колонки в таблицу
+            newTable.Columns.AddRange(coloum_0, coloum_1, coloum_2, coloum_3);
+
+
+            /* Заполняем ячейкии (Создаем строки) */
+
+            // Строка 1
+            DataGridViewCell Name1_0 = new DataGridViewTextBoxCell();
+            DataGridViewCell Name2_0 = new DataGridViewTextBoxCell();
+            DataGridViewCell Name3_0 = new DataGridViewTextBoxCell();
+            DataGridViewCell Name4_0 = new DataGridViewTextBoxCell();
+
+            Name1_0.Value = "Петров";
+            Name2_0.Value = "Иван";
+            Name3_0.Value = "Сергеевич";
+            Name4_0.Value = "00001";
+
+            DataGridViewRow row_0 = new DataGridViewRow();
+            row_0.Cells.AddRange(Name1_0, Name2_0, Name3_0, Name4_0);
+            newTable.Rows.Add(row_0);
+
+            // Строка 2
+            DataGridViewCell Name1_1 = new DataGridViewTextBoxCell();
+            DataGridViewCell Name2_1 = new DataGridViewTextBoxCell();
+            DataGridViewCell Name3_1 = new DataGridViewTextBoxCell();
+            DataGridViewCell Name4_1 = new DataGridViewTextBoxCell();
+
+            Name1_1.Value = "Сидоров";
+            Name2_1.Value = "Андрей";
+            Name3_1.Value = "Дмитриевич";
+            Name4_1.Value = "00002";
+
+            DataGridViewRow row_1 = new DataGridViewRow();
+            row_1.Cells.AddRange(Name1_1, Name2_1, Name3_1, Name4_1);
+            newTable.Rows.Add(row_1);
         }
     }
 }
